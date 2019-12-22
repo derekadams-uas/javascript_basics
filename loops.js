f13142a542fee545a27708511cec1142510adc1e
Derek Adams <derekadams.uas@gmail.com>
2019-12-22 09:42:29 -0700

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,22 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
+
+for (var i = 0; i < todos.length; i++) {
+    todos[i] = todos[i] + "!";
+}
+
+var counterTwo = 10;
+do {
+    console.log(counterTwo);
+    counterTwo--;
+} while (counterTwo > 0)
+
+
+todos.forEach(function (value, index) {
+    console.log(i);
+})
